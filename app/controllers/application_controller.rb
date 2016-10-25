8de920451f3a6272f0a1b37a4861a59ae4bc3069
Hongying LIU <dadaycm@hotmail.com>
2016-10-25 08:46:38 +0800

    Exercise C1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :getTimeNow
+
+  def getTimeNow
+    @date_loaded = Time.now
+    @string_now  = 'Now:'+Time.now.strftime("%Y-%m-%d %H:%M:%S")
+  end
 end
